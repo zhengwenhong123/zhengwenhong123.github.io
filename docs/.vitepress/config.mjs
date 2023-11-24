a59b12db348b76f09556a21a37b0567d366fcba9
Carmelo Witting <103306294+zhengwenhong123@users.noreply.github.com>
2023-11-24 16:44:58 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     outDir: '../public',
-    base: './',
+    base: '/vitepress-starter/',
     publicPath:'/vitepress-starter/',
     title: "我的第一个个人站点",
     description: "A VitePress Site",
