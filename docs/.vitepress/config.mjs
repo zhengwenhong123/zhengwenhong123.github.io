1022a87a49cc85cbe7dab6871f4c5c7eeea706cd
zwh <1716142352@qq.com>
2023-11-24 17:09:12 +0800

    测试新增

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     outDir: '../public',
-    base: './',
-    publicPath:'/vitepress-starter/',
+    base: '/vitepress/',
     title: "我的第一个个人站点",
     description: "A VitePress Site",
     themeConfig: {
